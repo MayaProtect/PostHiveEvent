0be4bae890db15740224d59a8ae52934b9413346
Ardjouni <samir.ardjouni@gmail.com>
2022-09-16 14:02:44 +0200

    feat: create method post add event

--- App/Event.py
+++ App/Event.py
@@ -1,35 +1,37 @@
-from flask import Flask, request, jsonify, Response
+from uuid import UUID
+import bson
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+CORS(app)
 
-class Event:
+host = os.environ.get("MONGO_HOST", "localhost")
+port = 27017
 
-@app.route('/event', methods=['GET'])
+client = MongoClient(host, port)
+
+db = client['mayaprotect']
+
+col = db['hives']
+
+
 @cross_origin()
-def register():
-    events = mongo.db.event.find()
-    response = json_util.dumps(events)
-    return  Response(response, mimetypes='application/json')
+@app.route('/')
+def hello():
+    return "Hello in my teams!"
 
 
-@app.route('/event/<id>', methods=['DELETE'])
-def delete_event(id)
+@cross_origin()
+@app.route('/hive/<hive_id>', methods=['POST'])
+def update_hive_event(hive_id):
+    data = request.get_json()
+    update_selector = {'uuid': bson.Binary.from_uuid(UUID(hive_id))}
+    update_data = {'$push': {'events': [data]}}
+    result = col.update_one(update_selector, update_data)
+    return flask.Response(status=201)
 
 
-@app.route('/event/<id>', methods=['PUT'])
-def update_event(id):
-    eventType = request.json['eventType']
-    description = request.json['description']
-
-    if eventType and description:
-        mongo.db.event.update_one({'_id': Event(id)}, {$set: {
-            'eventType': eventType,
-            'description': description
-        }})
-        response = jsonify({eventType + ' and ' + description + ' was updated successfully' })
-        return response
-
-
-
-if __name__ == '__main__':
-	app.run(debug=True)
+app.run(host="0.0.0.0", port=9001, debug=True)
